af66657f9c59818dbde532108b809bd07caa491a
Michail Borisenko <bmaa@yandex.ru>
2024-11-30 08:43:05 +0300

    301124 home

--- storage/storage.go
+++ storage/storage.go
@@ -1,5 +1,7 @@
 package storage
 
+// DB - PostgreSQL
+// driver - pq
 import (
 	"database/sql"
 	"fmt"
@@ -8,7 +10,6 @@ import (
 
 	//_ "modernc.org/sqlite"
 	_ "github.com/lib/pq" // Импорт драйвера
-	//"github.com/bmaayandexru/scheduler/tests"
 )
 
 type Task struct {
@@ -32,15 +33,12 @@ func NewTaskStore(db *sql.DB) TaskStore {
 	return TaskStore{DB: db}
 }
 
-// storage не должен выдавать sql.Result только error
-// func (ts TaskStore) Add(task Task) (sql.Result, error) {
 func (ts TaskStore) Add(task Task) error {
 	_, err := ts.DB.Exec("INSERT INTO scheduler(date, title, comment, repeat) VALUES ($1, $2, $3, $4) ",
 		task.Date, task.Title, task.Comment, task.Repeat)
 	return err
 }
 
-// func (ts TaskStore) Delete(id string) (sql.Result, error) {
 func (ts TaskStore) Delete(id string) error {
 	_, err := ts.DB.Exec("DELETE FROM scheduler WHERE id = $1", id)
 	return err
@@ -55,19 +53,20 @@ func (ts TaskStore) Find(search string) ([]Task, error) {
 	// возвращаем всё если строка пустая
 	if len(search) == 0 {
 		rows, err = ts.DB.Query("SELECT id, date, title, comment, repeat FROM scheduler ORDER BY date LIMIT $1", limit)
-	}
-	// парсим строку на дату
-	if date, err := time.Parse("02-01-2006", search); err == nil {
-		// дата есть
-		rows, err = ts.DB.Query("SELECT id, date, title, comment, repeat FROM scheduler WHERE date = $1 LIMIT $2",
-			date.Format(templ),
-			limit)
 	} else {
-		// даты нет
-		search = "%" + search + "%"
-		rows, err = ts.DB.Query("SELECT id, date, title, comment, repeat FROM scheduler WHERE UPPER(title) LIKE UPPER($1) OR UPPER(comment) LIKE UPPER($1) ORDER BY date LIMIT $2",
-			search,
-			limit)
+		// парсим строку на дату
+		if date, err := time.Parse("02-01-2006", search); err == nil {
+			// дата есть
+			rows, err = ts.DB.Query("SELECT id, date, title, comment, repeat FROM scheduler WHERE date = $1 LIMIT $2",
+				date.Format(templ),
+				limit)
+		} else {
+			// даты нет
+			search = "%" + search + "%"
+			rows, err = ts.DB.Query("SELECT id, date, title, comment, repeat FROM scheduler WHERE UPPER(title) LIKE UPPER($1) OR UPPER(comment) LIKE UPPER($1) ORDER BY date LIMIT $2",
+				search,
+				limit)
+		}
 	}
 	for rows.Next() {
 		task := Task{}
@@ -115,8 +114,6 @@ CREATE INDEX IF NOT EXISTS idx_title ON scheduler (title);
 func InitDBase(connStr string) (*sql.DB, error) {
 	var SqlDB *sql.DB
 	fmt.Println("Init Data Base...")
-	//	connStr := "user=postgres password=password dbname=dbscheduler sslmode=disable"
-	// connStr := tests.ConnStr
 	SqlDB, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Ошибка подключения: %v", err)
@@ -129,7 +126,6 @@ func InitDBase(connStr string) (*sql.DB, error) {
 	}
 	if _, err = SqlDB.Exec(schemaSQL); err != nil {
 		log.Fatalf("InitDB err: %v", err)
-		// SqlDB = nil
 		return SqlDB, err
 	}
 
